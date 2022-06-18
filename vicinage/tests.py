f37b3b721985ec3d50fb614669d55b7ba0325049
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-18 09:48:36 +0300

    create tests for the Hood class

--- vicinage/tests.py
+++ vicinage/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .models import Hood, Profile, Business, Post
+from django.contrib.auth.models import User
+
+
+user = User.objects.get(id=1)
+profile = Profile.objects.get(id=1)
 
 # Create your tests here.
+class HoodTestClass(TestCase):
+    def setUp(self):
+        self.newhood=Hood(name='ClayCity', location='Kasarani', county='Nairobi', image='default.png', admin='user')
+        self.newhood.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.newhood,Hood))
+
+    def test_save_image(self):
+        new_hood=self.new_hood
+        new_hood.create_hood()
+        posts=Hood.get_hoods()
+        self.assertTrue(len(posts)>0)
+
+    def update_image(self):
+        new_hood=self.new_hood
+        new_hood.update_hood()
+        posts=Hood.get_hoods()
+        self.assertTrue(len(posts)==0)
+
+    def test_delete_image(self):
+        new_hood=self.new_hood
+        new_hood.delete_hood()
+        posts=Hood.get_hoods()
+        self.assertTrue(len(posts)==0)
+
