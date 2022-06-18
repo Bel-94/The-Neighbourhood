90247d74191960bfc4e857a650f67625973a85f5
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-18 09:58:48 +0300

    create tests for the Business class

--- vicinage/tests.py
+++ vicinage/tests.py
@@ -9,8 +9,8 @@ profile = Profile.objects.get(id=1)
 # Create your tests here.
 class HoodTestClass(TestCase):
     def setUp(self):
-        self.newhood=Hood(name='ClayCity', location='Kasarani', county='Nairobi', image='default.png', admin='user')
-        self.newhood.save()
+        self.new_hood=Hood(name='ClayCity', location='Kasarani', county='Nairobi', image='default.png', admin='user')
+        self.new_hood.save()
 
     def test_instance(self):
         self.assertTrue(isinstance(self.newhood,Hood))
@@ -32,4 +32,35 @@ class HoodTestClass(TestCase):
         new_hood.delete_hood()
         posts=Hood.get_hoods()
         self.assertTrue(len(posts)==0)
+
+
+class TestBusiness(TestCase):
+    def setUp(self):
+        self.new_business=Business(name = "Cybafric", description="IT Company", email='info@cybafric.com', neighbourhood="Kasarani", owner=profile)
+        self.new_business.save()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_business,Business))
+
+    def test_save_image(self):
+        new_biz=self.new_business
+        new_biz.create_business()
+        posts=Business.get_businesses()
+        self.assertTrue(len(posts)>0)
+
+    def update_image(self):
+        new_biz=self.new_business
+        new_biz.update_business()
+        posts=Business.get_businesses()
+        self.assertTrue(len(posts)==0)
+
+    def test_delete_image(self):
+        new_biz=self.new_business
+        new_biz.delete_business()
+        posts=Business.get_businesses()
+        self.assertTrue(len(posts)==0)
+
+
+
+
 
