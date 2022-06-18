2958ca69bd608c2eac4396be7b1a7ee843dc9a51
Bel-94 <belinda.ntinyari@student.moringaschool.com>
2022-06-18 08:54:39 +0300

    create class hood in models file

--- vicinage/models.py
+++ vicinage/models.py
@@ -1,3 +1,97 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
+
+COUNTIES = [
+    ('', ('Choose')), 
+    ('Baringo', ('Baringo')),
+    ('Bomet', ('Bomet')),
+    ('Bungoma ', ('Bungoma ')),
+    ('Busia', ('Busia')),
+    ('Elgeyo Marakwet', ('Elgeyo Marakwet')),
+    ('Embu', ('Embu')),
+    ('Garissa', ('Garissa')),
+    ('Homa Bay', ('Homa Bay')),
+    ('Isiolo', ('Isiolo')),
+    ('Kajiado', ('Kajiado')),
+    ('Kakamega', ('Kakamega')),
+    ('Kericho', ('Kericho')),
+    ('Kiambu', ('Kiambu')),
+    ('Kilifi', ('Kilifi')),
+    ('Kirinyaga', ('Kirinyaga')),
+    ('Kisii', ('Kisii')),
+    ('Kisumu', ('Kisumu')),
+    ('Kitui', ('Kitui')),
+    ('Kwale', ('Kwale')),
+    ('Laikipia', ('Laikipia')),
+    ('Lamu', ('Lamu')),
+    ('Machakos', ('Machakos')),
+    ('Makueni', ('Makueni')),
+    ('Mandera', ('Mandera')),
+    ('Meru', ('Meru')),
+    ('Migori', ('Migori')),
+    ('Marsabit', ('Marsabit')),
+    ('Mombasa', ('Mombasa')),
+    ('Muranga', ('Muranga')),
+    ('Nairobi', ('Nairobi')),
+    ('Nakuru', ('Nakuru')),
+    ('Nandi', ('Nandi')),
+    ('Narok', ('Narok')),
+    ('Nyamira', ('Nyamira')),
+    ('Nyandarua', ('Nyandarua')),
+    ('Nyeri', ('Nyeri')),
+    ('Samburu', ('Samburu')),
+    ('Siaya', ('Siaya')),
+    ('Taita Taveta', ('Taita Taveta')),
+    ('Tana River', ('Tana River')),
+    ('Tharaka Nithi', ('Tharaka Nithi')),
+    ('Trans Nzoia', ('Trans Nzoia')),
+    ('Turkana', ('Turkana')),
+    ('Uasin Gishu', ('Uasin Gishu')),
+    ('Vihiga', ('Vihiga')),
+    ('Wajir', ('Wajir')),
+    ('West Pokot', ('West Pokot')),
+]
+
+
+
+class Hood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    county = models.CharField(choices=COUNTIES, max_length=50)
+    description = models.TextField(max_length=500)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='https://res.cloudinary.com/dz275mqsc/image/upload/v1654858776/default_nbsolf.png')
+    police_department = models.CharField(max_length=25, null=True)
+    health_department = models.CharField(max_length=25, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def get_hoods(self):
+        hoods = Hood.objects.all()
+        return hoods
+    
+    def create_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+    def find_hood(self,hood_id):
+        hood = Hood.objects.filter(self = hood_id)
+        return hood
+
+    def update_hood(self, id, name, location, county, image):
+        update = Hood.objects.filter(id = id).update(name = name , location = location, county = county, image = image)
+        return update
+
+    def __str__(self):
+        return str(self.name)
+    
+    class Meta:
+        verbose_name_plural = 'Hoods'
+
+    
--- vicinage/views.py
+++ vicinage/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import RegistrationForm
 
 # Create your views here.
+
+
+# function for the registration form
+# def register(request):
+#     if request.method=="POST":
+#         form=RegistrationForm(request.POST)
+#         procForm=profileForm(request.POST, request.FILES)
+#         if form.is_valid() and procForm.is_valid():
+#             username=form.cleaned_data.get('username')
+#             user=form.save()
+#             profile=procForm.save(commit=False)
+#             profile.user=user
+#             profile.save()
+
+#             # messages.success(request, f'Successfully created Account!.You can now login as {username}!')
+#         return redirect('login')
+#     else:
+#         form= RegistrationForm()
+#         prof=profileForm()
+#     params={
+#         'form':form,
+#         'profForm': prof
+#     }
+#     return render(request, 'registration/register.html', params)
+
 
 def index(request):
     return render(request, 'main/index.html')
